c8691518bbc7cc7750c5c1422d8b8c09f7ed2d23
Kadhim Dheyaa <ka7om@outlook.com>
2024-11-28 09:05:21 +0300

    changes to css and html and files

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.home, name='home'),
+    path('home', views.home, name='home'),
+    path("login", views.logIn, name="login"),
+    path("my-feedback", views.myFeedback),
+    path("feedback", views.feedback)
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "pages/home.html")
+
+def logIn(request):
+    return render(request, "pages/login_page.html")
+
+def myFeedback(request):
+    return render(request, "pages/my_feedback.html")
+
+def feedback(request):
+    return render(request, "pages/feedback.html")
